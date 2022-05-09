4abd84d93eeaec1d6ea3a7567e0d6bef9f008064
Steven <stevenbmendoza@gmail.com>
2022-05-08 17:19:05 -0700

    comment CRUD operations routes

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,49 @@
+const router = require("express").Router();
+const { Comment } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+// comment get /api/comments/
+router.get("/", (req, res) => {
+  Comment.findAll({})
+    .then((dbCommentData) => res.json(dbCommentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// comment post /api/comments/
+router.post("/", withAuth, (req, res) => {
+  // check if user is logged in by session
+  if (req.session) {
+    Comment.create({
+      comment_text: req.body.comment_text,
+      post_id: req.body.post_id,
+      // use the id from the user's logged in session
+      user_id: req.session.user_id,
+    })
+      .then((dbCommentData) => res.json(dbCommentData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  }
+});
+
+// delete a post /api/comments/:id
+router.delete("/:id", withAuth, (req, res) => {
+  Comment.destroy({ where: { id: req.params.id } })
+    .then((dbCommentData) => {
+      if (!dbCommentData) {
+        res.status(404).json({ message: "No comment found with this id" });
+        return;
+      }
+      res.json(dbCommentData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
