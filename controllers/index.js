709ac3025d042bf26b466106ce249cdb435db24d
Steven <stevenbmendoza@gmail.com>
2022-05-08 20:49:37 -0700

    home page and dashboard user routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 const apiRouter = require("./api");
+const homeRoutes = require("./home-routes");
+const dashboardRoutes = require("./dashboard-routes");
 
 router.use("/api", apiRouter);
+router.use("/", homeRoutes);
+router.use("/dashboard", dashboardRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
